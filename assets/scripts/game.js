c26dc56e45bb17d5c12949026174848159f7dc70
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-06-13 19:13:28 -0300

    basic structure

---
+++ assets/scripts/game.js
@@ -0,0 +1,10 @@
+class Game {
+  constructor(gameBoard) {
+    this.gameBoard = gameBoard;
+    this.hands = [
+      "./assets/images/paper.png",
+      "./assets/images/rock.png",
+      "./assets/images/scissors.png",
+    ];
+  }
+}
